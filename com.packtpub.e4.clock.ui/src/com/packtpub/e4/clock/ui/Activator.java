6a26f7e226114cea85b68d2e215f5cfc46d877c7
huyaye <huyaye@naver.com>
2018-02-11 11:14:24 +0900

    2 SWT로 뷰 작성

---
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/Activator.java
@@ -0,0 +1,152 @@
+package com.packtpub.e4.clock.ui;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.graphics.Region;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Tray;
+import org.eclipse.swt.widgets.TrayItem;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "com.packtpub.e4.clock.ui"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+
+	private TrayItem trayItem;
+	private Image image;
+	private Shell shell;
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.
+	 * BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+
+		/*
+		 * SWTException (Invald Thread) occurs when eclipse open.
+		 */
+		final Display display = Display.getDefault();
+		display.asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				image = new Image(display, Activator.class.getResourceAsStream("/icons/sample.gif"));
+				Tray tray = display.getSystemTray();
+				if (tray != null && image != null) {
+					trayItem = new TrayItem(tray, SWT.NONE);
+					trayItem.setToolTipText("Hello World");
+					trayItem.setVisible(true);
+					trayItem.setText("Hello World");
+					trayItem.setImage(image);
+
+					trayItem.addSelectionListener(new SelectionListener() {
+						@Override
+						public void widgetSelected(SelectionEvent e) {
+							if (shell == null || shell.isDisposed()) {
+								shell = new Shell(trayItem.getDisplay(), SWT.NO_TRIM | SWT.ON_TOP);
+								// shell.setAlpha(200);
+								shell.setLayout(new FillLayout());
+								new ClockWidget(shell, SWT.NONE, new RGB(255, 0, 255));
+								shell.pack();
+
+								final Region region = new Region();
+								region.add(circle(25, 25, 25));
+								shell.setRegion(region);
+								shell.addDisposeListener(new DisposeListener() {
+									@Override
+									public void widgetDisposed(DisposeEvent e) {
+										if (region != null && !region.isDisposed()) {
+											region.dispose();
+										}
+									}
+								});
+							}
+							shell.open();
+						}
+
+						@Override
+						public void widgetDefaultSelected(SelectionEvent e) {
+						}
+					});
+					trayItem.addDisposeListener(new DisposeListener() {
+						@Override
+						public void widgetDisposed(DisposeEvent e) {
+							image.dispose();
+						}
+					});
+				}
+			}
+		});
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.
+	 * BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path
+	 *
+	 * @param path
+	 *            the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+
+	private static int[] circle(int r, int offsetX, int offsetY) {
+		int[] polygon = new int[8 * r + 4];
+
+		for (int i = 0; i < 2 * r + 1; i++) {
+			int x = i - r;
+			int y = (int) Math.sqrt(r * r - x * x);
+			polygon[2 * i] = offsetX + x;
+			polygon[2 * i + 1] = offsetY + y;
+			polygon[8 * r - 2 * i - 2] = offsetX + x;
+			polygon[8 * r - 2 * i - 1] = offsetY - y;
+		}
+		return polygon;
+	}
+}
---
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/ClockWidget.java
@@ -0,0 +1,100 @@
+package com.packtpub.e4.clock.ui;
+
+import java.util.Date;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+
+public class ClockWidget extends Canvas {
+	private Thread tickTock;
+	private final Color color;
+	private int offset;
+
+	public ClockWidget(Composite parent, int style, RGB rgb) {
+		super(parent, style);
+
+		this.color = new Color(parent.getDisplay(), rgb);
+
+		tickTock = new Thread("TickTock") {
+			@Override
+			public void run() {
+				while (!ClockWidget.this.isDisposed()) {
+					ClockWidget.this.getDisplay().asyncExec(new Runnable() {
+						@Override
+						public void run() {
+							if (!ClockWidget.this.isDisposed()) {
+								ClockWidget.this.redraw();
+							}
+						}
+					});
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						return;
+					}
+				}
+			}
+		};
+
+		addPaintListener(new PaintListener() {
+			@Override
+			public void paintControl(PaintEvent e) {
+				ClockWidget.this.paintControl(e);
+			}
+		});
+
+		addDisposeListener(new DisposeListener() {
+			@Override
+			public void widgetDisposed(DisposeEvent e) {
+				if (color != null && !color.isDisposed()) {
+					color.dispose();
+				}
+			}
+		});
+
+		tickTock.start();
+	}
+
+	public void setOffset(int offset) {
+		this.offset = offset;
+	}
+
+	@Override
+	public Point computeSize(int w, int h, boolean changed) {
+		int size;
+		if (w == SWT.DEFAULT) {
+			size = h;
+		} else if (h == SWT.DEFAULT) {
+			size = w;
+		} else {
+			size = Math.min(w, h);
+		}
+		if (size == SWT.DEFAULT) {
+			size = 50;
+		}
+		return new Point(size, size);
+	}
+
+	private void paintControl(PaintEvent e) {
+		e.gc.drawArc(e.x, e.y, e.width - 1, e.height - 1, 0, 360);
+
+		int seconds = new Date().getSeconds();
+		int arc = (15 - seconds) * 6 % 360;
+		e.gc.setBackground(color);
+		e.gc.fillArc(e.x, e.y, e.width - 1, e.height - 1, arc - 1, 2);
+
+		e.gc.setBackground(e.display.getSystemColor(SWT.COLOR_BLACK));
+		int hours = new Date().getHours() + offset;
+		arc = (3 - hours) * 30 % 360;
+		e.gc.fillArc(e.x, e.y, e.width - 1, e.height - 1, arc - 5, 10);
+
+	}
+}
